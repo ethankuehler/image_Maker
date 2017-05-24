3a0935b169c188be32d9e5dd6cdf5f77e91dc87f
TEC25DU <TEC25DU@students.sunywcc.edu>
2017-05-24 12:34:12 -0400

    updateing main

--- image_maker/image_maker/Main.cpp
+++ image_maker/image_maker/Main.cpp
@@ -3,35 +3,42 @@
 #include <memory>
 #include <functional>
 
+const float PI = 3.14159265359;
+const float DTOR = PI / 180;
+
 std::unique_ptr<BMP> Make_Image(std::function<RGBApixel(int,int)> Func, int width, int hight);
 
-
 int main(int argc, char* argv[]) {
-  auto func = [](int x, int y) -> RGBApixel {
-	RGBApixel color;
-    color.Red = (x % 255);
-    color.Blue = (y % 255);
-    color.Green = ((x+y)%255);
-    return color;
-  };
+	auto func = [](int x, int y) -> RGBApixel {
+		++y;
+		++x;
 
-  std::unique_ptr<BMP> image = Make_Image(func, 150, 150);
+		int color = tan((x * y) * DTOR) * 255;
+		int other = tan((x * y) * DTOR) * 255;
+		int fith = tan((y * x) * DTOR) * 255;
+
+		RGBApixel pixel;
+		pixel.Red = color;
+		pixel.Blue = other;
+		pixel.Green = fith;
+		return pixel;
+	};
+
+	std::unique_ptr<BMP> image = Make_Image(func, 1920, 1080);
   
-  image->WriteToFile(argv[1]);
+	image->WriteToFile("C:/My Documents/visual studio 2015/Projects/image_Maker/image_maker/Debug/test.bmp");
 
-  return 0;
+	return 0;
 }
 
-template <typename Func>
 std::unique_ptr<BMP> Make_Image(std::function<RGBApixel(int, int)> Func, int width, int hight) {
-  std::unique_prt<BMP> image = std::make_unique<bmp>();
-  image->SetSize(width, hight);
+	std::unique_ptr<BMP> image = std::make_unique<BMP>();
+	image->SetSize(width, hight);
 
-  for(int x = 0; x < width; x++) {
-    for(int y = 0; y < hight; y++) {
-      image.SetPixel(x, y, Func(x,y));
-    }
-  }
-
-  return image;
+	for(int x = 0; x < width; x++) {
+		for(int y = 0; y < hight; y++) {
+			image->SetPixel(x, y, Func(x,y));
+		}
+	}
+	return image;
 }
